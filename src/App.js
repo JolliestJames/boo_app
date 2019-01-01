baed299d423ec5f6cc9c7973d8328db10017b1ca
JolliestJames <cloudismydog@gmail.com>
2018-12-31 17:31:56 -0800

    Rewrite root component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Loading } from './components/common';
+import Auth from './screens/Auth';
+import LoggedIn from './screens/LoggedIn';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      jwt: ''
+    }
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+    if (!this.state.jwt) {
+      return (
+        <Auth />
+      );
+    } else if (this.state) {
+      return (
+        <LoggedIn />
+      );
+    }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
